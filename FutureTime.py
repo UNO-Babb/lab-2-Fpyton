8d9b4ef8341fa797aacf48193b5af2df672610cf
Fpyton <fmolina@unomaha.edu>
2025-09-04 15:03:29 +0000

    Done with Magic8Ball

--- FutureTime.py
+++ FutureTime.py
@@ -9,7 +9,7 @@ import datetime
 def main():
   #getting current time from system, storing to variable
   now = datetime.datetime.now()
-  currentHour = now.hour
+  currentHour = (now.hour - 5) % 24
   currentMinute = now.minute
 
   print (currentHour, currentMinute) #this is just for checking, we should delete it later
@@ -17,9 +17,15 @@ def main():
   #TODO:
   #Ask user for hours
   #Ask user for minutes
+  moreMins = 25
 
   #Calculate the time after the user-supplied time has passed.
+  futureMins = (currentMinute + moreMins) % 60
+  extraHour = (currentMinute + moreMins) // 60
+  print(extraHour)
 
+future_minutes = ("totalMinutes" + "addtotalMinutes") % (24 * 60)
+  print(future_minutes)
   #Do not use any if statements in calculating the time.
 
   #Output the future time in standard format "HH:MM"
